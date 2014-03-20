20a33ed96f73ba90f69537de95aebec488ea566f
sara <sjsshop@yahoo.com>
2014-03-19 21:10:28 -0600

    Partially complete tablet/phone implementation

--- src/com/cs646/photosharing/PhotoListActivity.java
+++ src/com/cs646/photosharing/PhotoListActivity.java
@@ -1,11 +1,28 @@
 package com.cs646.photosharing;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 
-public class PhotoListActivity extends SingleFragmentActivity {
+public class PhotoListActivity extends FragmentActivity {
 
-	@Override
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photolist_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
+    
 	public Fragment createFragment() {
 		// Get the user id from the intent and pass it
 		// to the Fragment using Fragment Arguments
--- src/com/cs646/photosharing/UserListActivity.java
+++ src/com/cs646/photosharing/UserListActivity.java
@@ -1,12 +1,30 @@
 package com.cs646.photosharing;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class UserListActivity extends SingleFragmentActivity {
+public class UserListActivity extends FragmentActivity {
 	public static final String TAG = "PhotoSharing";
 
-	@Override
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userlist_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
+    
     public Fragment createFragment() {
         return new UserListFragment();
     }
+	
 }
